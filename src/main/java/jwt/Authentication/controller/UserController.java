35335b0d7585acffb5706b88f925e20cba09d02c
sai krishna <86594513+saikrishnacheruku@users.noreply.github.com>
2025-07-24 16:30:06 +0530

    first commit

---
+++ src/main/java/jwt/Authentication/controller/UserController.java
@@ -0,0 +1,25 @@
+package jwt.Authentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping({"/"})
+    public String getMessage(){
+        return "Welcome to  screen ";
+    }
+
+    @GetMapping({"/admin"})
+    public String getAdminMessage(){
+        return "Welcome to  Admin screen ";
+    }
+
+    @GetMapping({"/user"})
+    public String getUserMessage(){
+        return "Welcome to  screen ";
+    }
+
+}
---
+++ src/test/java/jwt/Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt.Authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
